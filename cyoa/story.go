92b41c29b2f5e3230d3338c609e9be16997eed2f
coolabhays <abhaysp9955@gmail.com>
2022-03-28 18:15:27 +0530

    add handling for different path pattern which represent story chapters

--- cyoa/story.go
+++ cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -21,16 +23,58 @@ var defaultStoryTmpl = `
 		<title>{{.Title}}</title>
 	</head>
 	<body>
-		<h1>{{.Title}}</h1>
-		{{range .Paragraphs}}
-		<p>{{.}}</p>
-		{{end}}
-		<br>
-		<ul>
-			{{range .Options}}
-			<li><a href="/{{.Arc}}">{{.Text}}</a></li>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+			<p>{{.}}</p>
 			{{end}}
-		</ul>
+			<br>
+			<ul>
+				{{range .Options}}
+				<li><a href="/{{.Arc}}">{{.Text}}</a></li>
+				{{end}}
+			</ul>
+		</section>
+		<style>
+			body {
+				font-family: Source Code Pro;
+			}
+			h1 {
+				text-align: center;
+				position: relative;
+			}
+			.page {
+				width: 80%;
+				max-width: 750px;
+				margin: auto;
+				margin-top: 40px;
+				margin-bottom: 40px;
+				padding: 40px;
+				background: #e2ddff;
+				border: 1px solid #eee;
+				box-shadow: 5px 10px #333;
+			}
+			ul {
+				border-top: 1px dotted #ccc;
+				padding: 10px 0 0 0;
+				-webkit-padding-start: 0;
+			}
+			li {
+				padding: 10px;
+			}
+			a,
+			a:visited {
+				text-decoration: none;
+				color: #659ef8;
+			}
+			a,
+			a:hover {
+				color: #319aff;
+			}
+			p {
+				text-indent: 1em;
+			}
+		</style>
 	</body>
 </html>`
 
@@ -43,10 +87,20 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// there's always going to be an "intro" in the story
-	if err := tpl.Execute(w, h.s["intro"]); err != nil {
-		panic(err)
+	path := strings.TrimSpace(req.URL.Path)
+
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+
+	if chapter, ok := h.s[path[1:]]; ok {
+		if err := tpl.Execute(w, chapter); err != nil {
+			log.Printf("%v\n", err)
+			http.Error(w, "something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "chapter not found.", http.StatusNotFound)
 }
 
 type Chapter struct {

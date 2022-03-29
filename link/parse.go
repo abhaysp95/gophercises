791bb2a87ad74060a61d7125cbce7d24d755729c
coolabhays <abhaysp9955@gmail.com>
2022-03-29 19:52:07 +0530

    add the dfs logic to parse the html tree and provide []Link

--- link/parse.go
+++ link/parse.go
@@ -1,6 +1,12 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"golang.org/x/net/html"
+)
 
 // Link contains data of link <a href="">""</a>
 type Link struct {
@@ -11,5 +17,34 @@ type Link struct {
 // Parse will take an html document and will return slice of
 // Link parsed from it or error
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+	rootNode, err := html.Parse(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var links []Link
+	var f func(*html.Node, string) string
+	f = func(n *html.Node, pad string) string {
+		var data string
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			data = f(c, pad + "  ")
+		}
+		fmt.Printf("\t%s%+v\n", pad, n.Data)
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println("here")
+					links = append(links, Link{
+						Href: a.Val,
+						Text: data,
+					})
+					break  // other attributes of <a></a> tag doesn't matter
+				}
+			}
+		}
+		return n.Data
+	}
+	f(rootNode, "")
+
+	return links, nil
 }

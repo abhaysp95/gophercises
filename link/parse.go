eb3af364d33d7247798e80817ff6c3efabd1d1fd
coolabhays <abhaysp9955@gmail.com>
2022-03-29 20:14:21 +0530

    update the parser with better formatted approach

--- link/parse.go
+++ link/parse.go
@@ -23,28 +23,68 @@ func Parse(r io.Reader) ([]Link, error) {
 	}
 
 	var links []Link
-	var f func(*html.Node, string) string
-	f = func(n *html.Node, pad string) string {
-		var data string
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			data = f(c, pad + "  ")
-		}
-		fmt.Printf("\t%s%+v\n", pad, n.Data)
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					fmt.Println("here")
-					links = append(links, Link{
-						Href: a.Val,
-						Text: data,
-					})
-					break  // other attributes of <a></a> tag doesn't matter
-				}
-			}
-		}
-		return n.Data
+	nodes := linkNodes(rootNode)
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+		fmt.Println(node)
 	}
-	f(rootNode, "")
 
 	return links, nil
 }
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, a := range n.Attr {
+		if a.Key == "href" {
+			ret = Link{Href: a.Val}
+			break  // ignore other attributes
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			ret.Text = c.Data
+			break
+		}
+	}
+	return ret
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+
+	var nodes []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		nodes = append(nodes, linkNodes(c)...)
+	}
+	return nodes
+}
+
+
+// Initial logic for generalized parsing of links with href and their text
+/* var links []Link
+var f func(*html.Node, string) string
+f = func(n *html.Node, pad string) string {
+	var data string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		data = f(c, pad + "  ")
+	}
+	fmt.Printf("\t%s%+v\n", pad, n.Data)
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				fmt.Println("here")
+				links = append(links, Link{
+					Href: a.Val,
+					Text: data,
+				})
+				break  // other attributes of <a></a> tag doesn't matter
+			}
+		}
+	} else if n.Type == html.TextNode {
+		return n.Data
+	}
+	return ""
+}
+f(rootNode, "") */

60f8ea18e0e6832b21450cbca23ef91403b41097
coolabhays <abhaysp9955@gmail.com>
2022-04-03 14:07:14 +0530

    feat: add call to db.Init() to initialize db setup

--- task/main.go
+++ task/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "gophercises/task/cmd"
+import (
+	"gophercises/task/cmd"
+	"gophercises/task/db"
+	"log"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
